f44366e0e811ac16608db3b47303d59da4d54b48
Vitor Lourenço <50245199+lourencovitor@users.noreply.github.com>
2020-04-23 00:38:45 -0300

    Aprendendo Node.Js

---
+++ 03-events/index.js
@@ -0,0 +1,24 @@
+const EventEmitter = require("events");
+
+class MeuEmissor extends EventEmitter {}
+
+const meuEmissor = new MeuEmissor();
+
+const nomeEvento = "usuario:click";
+
+meuEmissor.on(nomeEvento, function (click) {
+  console.log("Um usuario clicou", click);
+});
+
+// meuEmissor.emit(nomeEvento, "na barra de rolagem");
+// meuEmissor.emit(nomeEvento, "no ok");
+
+// let count = 0;
+// setInterval(function () {
+//   meuEmissor.emit(nomeEvento, "no ok" + count++);
+// }, 1000);
+
+const stdin = process.openStdin();
+stdin.addListener("data", function (value) {
+  console.log(`Você digitou: ${value.toString().trim()}`);
+});
