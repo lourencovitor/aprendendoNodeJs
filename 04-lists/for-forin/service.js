ead52bdc11240a47b8da0d04dbd4cbae4a6c471d
Vitor Lourenço <50245199+lourencovitor@users.noreply.github.com>
2020-05-18 23:04:26 -0300

    manipulando testes automatizados

--- 04-lists/for-forin/service.js
+++ 04-lists/for-forin/service.js
@@ -1,20 +1,12 @@
-const axios = require("axios");
-const URL = `https://swapi.co/api/people`;
+const axios = require('axios')
+const URL = `https://swapi.co/api/people`
 
 async function obterPessoas(nome) {
-  const url = `${URL}/?search=${nome}&format=json`;
-  const response = await axios.get(url);
-  return response.data;
+    const url = `${URL}/?search=${nome}&format=json`
+    const response = await axios.get(url)
+    return response.data
 }
 
-obterPessoas("r2")
-  .then(function (resultado) {
-    console.log("resultado", resultado);
-  })
-  .catch(function (error) {
-    console.log("DEU RUIM", error);
-  });
-
 module.exports = {
-  obterPessoas,
-};
+    obterPessoas
+}
